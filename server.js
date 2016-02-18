3355d53c389b55fe29b16560bf9b26e3471926f8
jacobpaine <painejacob@gmail.com>
2016-02-18 13:41:20 -0600

    Destroy enabled

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-'use strict'
+'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require("method-override");
+
+const note = require('./routes/note');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get(`/`, (req, res) => {
+app.set('view engine', 'jade');
+
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get('/', (req, res) => {
   res.send('Server Running');
 });
 
-app.listen(PORT, () => {
-  console.log(`Evernode server ${PORT}`);
-})
+app.use(note);
+
+mongoose.connect('mongodb://localhost:27017/evernode', (err) => {
+  if (err) throw err;
+
+  app.listen(port, () => {
+    console.log(`Evernode server running on port: ${port}`);
+  });
+});
