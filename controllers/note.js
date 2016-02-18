f4364bcd3fc9403137170ee40bdf12bad13890a2
jacobpaine <painejacob@gmail.com>
2016-02-18 15:41:13 -0600

    ES6 stuff

--- controllers/note.js
+++ controllers/note.js
@@ -2,55 +2,48 @@
 
 const Note = require('../models/note');
 
-module.exports.edit = (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
+module.exports = {
+  edit (req, res) {
+    res.render('new-note', {note: req.note});
+  },
 
-    res.render('new-note', {note: note});
-  });
-};
+  update (req, res) {
+    req.note.update(req.body, (err) => {
+      if (err) throw err;
 
-module.exports.update = (req, res) => {
-  Note.findByIdAndUpdate(req.params.id,
-    req.body, (err, note) => {
-    if (err) throw err;
+      res.redirect(`/notes/${req.note._id}`);
+    });
+  },
 
-    res.redirect(`/notes/${note._id}`);
-  });
-};
+  index (req, res) {
+    Note.find({}, (err, notes) => {
+      if (err) throw err;
 
-module.exports.index = (req, res) => {
-  Note.find({}, (err, notes) => {
-    if (err) throw err;
+      res.render('notes-index', {notes: notes});
+    });
+  },
 
-    res.render('notes-index', {notes: notes});
-  });
-};
+  newNote (req, res) {
+    res.render('new-note');
+  },
 
-module.exports.newNote = (req, res) => {
-  res.render('new-note');
-};
+  show (req, res) {
+    res.render('show-note', {note: req.note});
+  },
 
-module.exports.show = (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
+  create (req, res) {
+    Note.create(req.body, (err, note) => {
+      if (err) throw err;
 
-    res.render('show-note', {note: note});
-  });
-};
+      res.redirect(`/notes/${note._id}`);
+    });
+  },
 
-module.exports.create = (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
+  destroy (req, res) {
+    req.note.remove((err) => {
+      if (err) throw err;
 
-    res.redirect(`/notes/${note._id}`);
-  });
-};
-
-module.exports.destroy = (req, res) => {
-  Note.findByIdAndRemove(req.params.id, (err) => {
-    if (err) throw err;
-
-    res.redirect('/notes');
-  });
+      res.redirect('/notes');
+    });
+  }
 };
--- routes/note.js
+++ routes/note.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const Note = require('../models/note');
 const note = require('../controllers/note');
+
+router.param('id', (req, res, next, id) => {
+  Note.findById(id, (err, note) => {
+    if (err) throw err;
+
+    req.note = note;
+    next();
+  });
+});
 
 router.get('/notes', note.index);
 router.get('/notes/new', note.newNote);
 router.get('/notes/:id', note.show);
-router.delete('/notes/:id', note.destroy);
-router.post('/notes', note.create);
 router.get('/notes/:id/edit', note.edit);
 router.put('/notes/:id', note.update);
-
+router.delete('/notes/:id', note.destroy);
+router.post('/notes', note.create);
 
 module.exports = router;
